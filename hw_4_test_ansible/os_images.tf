a64b5273bd2296dc2b4e41f7f5f72cf8bb2ae13a
mikhail.timofeev <mikhail.timofeev@lgepartner.com>
2023-11-07 18:25:28 +0300

    it's work

---
+++ hw_4_test_ansible/os_images.tf
@@ -0,0 +1,12 @@
+data "yandex_compute_image" "lamp" {
+  family = "lamp"
+}
+
+
+data "yandex_compute_image" "lemp" {
+  family = "lemp"
+}
+
+data "yandex_compute_image" "ubuntu-20" {
+  family = "ubuntu-2004-lts"
+}
---
+++ hw_4_test_ansible/wp-app.tf
@@ -0,0 +1,49 @@
+resource "yandex_compute_instance" "wp-app-1" {
+  name = "wp-app-1"
+  zone = "ru-central1-a"
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-20.id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = yandex_vpc_subnet.wp-subnet-a.id
+    nat       = true 
+  }
+
+    metadata = {
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+}
+
+resource "yandex_compute_instance" "wp-app-2" {
+  name = "wp-app-2"
+  zone = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+   boot_disk {
+     initialize_params {
+       image_id = data.yandex_compute_image.ubuntu-20.id
+    }
+}
+    network_interface {
+      subnet_id = yandex_vpc_subnet.wp-subnet-b.id
+      nat       = true
+    }
+
+   metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+}
